59fd4d287f56a82173a8ba025674905f6f440ac0
hugo hadfield <hadfield.hugo@gmail.com>
2020-03-05 18:42:33 +0000

    Added useful information output

--- readable_benchmark.py
+++ readable_benchmark.py
@@ -1,5 +1,16 @@
 import pstats
 from pstats import SortKey
+import sys
+import clifford
+import numpy as np
+import numba
+
+
+print('Python version: ' , sys.version)
+print('Clifford version: ', clifford.__version__)
+print('Numpy version: ', np.__version__)
+print('Numba version: ', numba.__version__)
+
 
 p = pstats.Stats('benchmark.prof')
 p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats(400)
