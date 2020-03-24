a7cda6e99f42ce1da6a28803a4f84729f42bf1c6
hugo hadfield <hadfield.hugo@gmail.com>
2020-03-24 08:30:29 +0000

    Added generation and rendering of textured KB splines

--- splines.py
+++ splines.py
@@ -2,6 +2,8 @@
 from clifford.tools.g3c import *
 from clifford.g3c import *
 from pyganja import *
+from meshing import *
+from derivatives import *
 
 
 def reject_Cdot(C, Cdot):
@@ -70,13 +72,37 @@ def test_derivative_hermite_curve():
         np.testing.assert_allclose(dspline[1:fin], mdiff[1:fin], 1E-2, 1E-6)
 
 
+def derivative_projected_KB_curve(X0, V0, X1, V1, alpha):
+    """
+    The derivative of the KB curve on the blade manifold at
+    evolution parameter value alpha
+    """
+    Xdash = hermite_curve(X0, V0, X1, V1, alpha)
+    dXdashdalpha = derivative_hermite_curve(X0, V0, X1, V1, alpha)
+    dXda = differential_manifold_projection(Xdash, dXdashdalpha)(3).normal()
+    return dXda
+
+
+def evolved_circle_normal(C, Cdot, X):
+    """
+    Get the normal to an evolved circle at X given C and Cdot
+    """
+    LC = (X|C)^einf
+    LT = (X|(C*Cdot))^X^einf
+    return normalised((LT*LC*I5)(3))
+
+
 def projected_KB_spline(control_list,
                         tension=0, 
                         bias=0, 
                         continuity=0,
+                        nalpha=20,
                         reject_Cdot_flag=False):
+    """
+    Generates a KB curve projected onto the object manifold
+    """
     output_objects = []
-
+    derivative_objects = []
     npoints = len(control_list)
     for i in range(1,npoints-2):
         previous_point = control_list[i-1]
@@ -100,11 +126,13 @@ def projected_KB_spline(control_list,
             V0 = di
             V1 = dip
 
-        for alpha in np.linspace(0, 1, 20):
-            C = average_objects([hermite_curve(X0, V0, X1, V1, alpha)])(3).normal()
-            print(get_circle_in_euc(C))
+        for alpha in np.linspace(0, 1, nalpha):
+            Xdash = hermite_curve(X0, V0, X1, V1, alpha)
+            deriv = derivative_projected_KB_curve(X0, V0, X1, V1, alpha)
+            C = average_objects([Xdash])(3).normal()
             output_objects.append(C)
-    return output_objects
+            derivative_objects.append(deriv)
+    return output_objects, derivative_objects
 
 
 def test_projected_KB_spline():
@@ -117,15 +145,55 @@ def test_projected_KB_spline():
     D = generate_dilation_rotor(0.8)
     T = ((1 - 0.5*(0.1*e2)*einf)*generate_rotation_rotor(np.pi/6, e1, e3)*D).normal()
     control_list = [apply_rotor(Cstart, ((T**n)*Tstart)(0,2,4).normal())(3).normal() for n in range(nobjs)]
-    spline_objs = projected_KB_spline(control_list)
+    spline_objs, derivative_objects = projected_KB_spline(control_list)
     gs = GanjaScene()
     gs.add_objects(spline_objs, color=Color.BLACK)
     gs.add_objects(control_list, color=Color.RED)
     draw(gs, scale=1.0, browser_window=True)
 
 
+def test_mesh_projected_KB_spline():
+
+    # Generate the circle spline
+    size = 0.1
+    t = 1.0*e1
+    nobjs = 6
+    Cstart = (up(size*1.0*e1)^up(size*1.0*e2)^up(-size*1.0*e1)).normal()
+    Tstart = 1 - 0.5*t*einf
+    D = generate_dilation_rotor(0.8)
+    T = ((1 - 0.5*(0.1*e2)*einf)*generate_rotation_rotor(np.pi/6, e1, e3)*D).normal()
+    control_list = [apply_rotor(Cstart, ((T**n)*Tstart)(0,2,4).normal())(3).normal() for n in range(nobjs)]
+
+    spline_objs, derivative_objects = projected_KB_spline(control_list)
+
+    # Mesh the spline
+    n_points = 21
+    n_alpha = len(spline_objs)
+    vertex_list, texture_coords = vertex_circles(spline_objs, n_points)
+    face_list = mesh_grid(n_alpha, n_points, mask=None, loopx=True)
+
+    # Get the vetex normals
+    vertex_normals = []
+    for i in range(len(vertex_list)):
+        X = vertex_list[i]
+        cind = int(i/(n_points+1))
+        C = spline_objs[cind]
+        Cdot = derivative_objects[cind]
+        norm_line = evolved_circle_normal(C, Cdot, X)
+        norm_val = -((norm_line*I5)(e123)*I3).normal().value[1:4]
+        vertex_normals.append(norm_val)
+
+    # Write the file
+    threedvertexlist = [down(v).value[1:4] for v in vertex_list]
+    write_obj_file('surfaces/KBSpline.obj', threedvertexlist, face_list, 
+                    vertex_normals=vertex_normals, 
+                    texture_coords=texture_coords,
+                    use_mtl=True)
+
+
 if __name__ == '__main__':
     # test_reject_Cdot()
     # test_derivative_hermite_spline()
-    test_projected_KB_spline()
+    # test_projected_KB_spline()
+    test_mesh_projected_KB_spline()
     pass
